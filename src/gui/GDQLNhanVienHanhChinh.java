679eea46bc6b68b76ec779faa8a42aba3929b790
Quan Pham <93675627+phamhoangngocquan2805@users.noreply.github.com>
2023-11-19 10:12:51 +0700

    push gd qlnvhc

--- src/gui/GDQLNhanVienHanhChinh.java
+++ src/gui/GDQLNhanVienHanhChinh.java
@@ -23,7 +23,7 @@ import javax.swing.table.DefaultTableModel;
 
 /**
  *
- * @author Ngọc Quân
+ * @author Ngọc Quân dev
  */
 public class GDQLNhanVienHanhChinh extends javax.swing.JPanel implements MouseListener {
 
